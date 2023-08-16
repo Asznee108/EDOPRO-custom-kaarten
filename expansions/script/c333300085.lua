453f2e12680483e02b5a8c2856c49f1917b156b2
Asznee108 <naiem786@hotmail.com>
2023-08-16 12:41:04 +0200

    Inferno King Red Dread (added)

---
+++ expansions/script/c333300085.lua
@@ -0,0 +1,116 @@
+--Dread King Red Inferno
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	--Synchro Summon Procedure
+	Synchro.AddProcedure(c,nil,1,1,Synchro.NonTuner(nil),1,99)
+	--Gains 200 ATK per Fiend monster
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetValue(s.atkval)
+	c:RegisterEffect(e1)
+    --Excavate equal to the number of Fiend monsters controlled +2, add 1 Fiend to hand/to GY
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+    e2:SetCountLimit(1,id)
+	e2:SetTarget(s.exctg)
+	e2:SetOperation(s.excop)
+	c:RegisterEffect(e2)
+    --Special Summon Fiend monsters
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_DESTROY)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+	e3:SetCode(EVENT_LEAVE_FIELD)
+    e3:SetCountLimit(1,{id,1})
+	e3:SetCondition(s.spcon)
+    e3:SetTarget(s.sptg)
+	e3:SetOperation(s.spop)
+	c:RegisterEffect(e3)
+end
+--Gain ATK
+function s.atkval(e,c)
+	return Duel.GetMatchingGroupCount(Card.IsRace,c:GetControler(),LOCATION_GRAVE,0,nil,RACE_FIEND)*300
+end
+--Excavate
+function s.exctg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		local ct=Duel.GetMatchingGroupCount(aux.FaceupFilter(Card.IsRace,RACE_FIEND),tp,LOCATION_MZONE,0,nil)+2
+		if Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)<ct then return false end
+		local g=Duel.GetDecktopGroup(tp,ct)
+		return g:FilterCount(Card.IsAbleToHand,nil)>0
+	end
+	Duel.SetPossibleOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+    Duel.SetPossibleOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
+end
+function s.thfilter(c)
+	return c:IsMonster() and c:IsRace(RACE_FIEND) and (c:IsAbleToHand() or c:IsAbleToGrave())
+end
+	--Excavate equal to the number of Fiend monsters controlled +2, add 1 Fiend to hand whose level is equal/less than number of revealed cards
+function s.excop(e,tp,eg,ep,ev,re,r,rp)
+	local ct=Duel.GetMatchingGroupCount(aux.FaceupFilter(Card.IsRace,RACE_FIEND),tp,LOCATION_MZONE,0,nil)+2
+	if Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)<ct then return end
+	Duel.ConfirmDecktop(tp,ct)
+	local g=Duel.GetDecktopGroup(tp,ct)
+	local db=0
+	local dc=g:Filter(s.thfilter,nil,ct)
+	if #dc>0 then
+		Duel.DisableShuffleCheck()
+		local sg=dc:Select(tp,1,1,nil)
+		aux.ToHandOrElse(sg,tp)
+		db=1
+	end
+	local ac=ct-db
+	if ac>0 then
+		Duel.SortDecktop(tp,tp,ac)
+	end
+end
+--Special Summon Fiend monsters
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsPreviousLocation(LOCATION_MZONE) and c:IsSummonType(SUMMON_TYPE_SYNCHRO)
+		and rp==1-tp and c:IsPreviousControler(tp)
+end
+function s.spfilter(c,e,sp)
+	return c:IsLevelBelow(4) and c:IsRace(RACE_FIEND) and c:IsCanBeSpecialSummoned(e,0,sp,false,false)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,0,LOCATION_GRAVE)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	local tg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_GRAVE,0,nil,e,tp)
+	if ft<=0 or #tg==0 then return end
+	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ft=1 end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=tg:Select(tp,ft,ft,nil)
+	local c=e:GetHandler()
+	local tc=g:GetFirst()
+	for tc in aux.Next(g) do
+		Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP)
+		-- Cannot Special Summon non-Synchro monsters from Extra Deck
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+		e1:SetRange(LOCATION_MZONE)
+		e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+		e1:SetAbsoluteRange(tp,1,0)
+		e1:SetTarget(function(_,c) return c:IsLocation(LOCATION_EXTRA) and not c:IsType(TYPE_SYNCHRO) end)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e1,true)
+		-- Lizard check
+		local e2=aux.createContinuousLizardCheck(c,LOCATION_MZONE,function(_,c) return not c:IsOriginalType(TYPE_SYNCHRO) end)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e2,true)
+	end
+	Duel.SpecialSummonComplete()
+end
