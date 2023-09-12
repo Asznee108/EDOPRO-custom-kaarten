f7fd94e06fea5cc658d27f2c41bc8d0787b8de8e
Asznee108 <naiem786@hotmail.com>
2023-09-12 23:52:50 +0200

    added: Red Invalidation

---
+++ expansions/script/c333300060.lua
@@ -0,0 +1,70 @@
+--Red Invalidation
+Duel.LoadScript("c420.lua")
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,TIMING_END_PHASE)
+	e1:SetCondition(s.condition)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+    --protect from battle
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCost(aux.bfgcost)
+	e2:SetOperation(s.battleop)
+	c:RegisterEffect(e2)
+end
+s.listed_series={0x543}
+s[0]=0
+s[1]=0
+--protect from effects
+function s.cfilter(c)
+	return c:IsFaceup() and c:IsRed()
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e1:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+	e1:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e1:SetValue(1)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+    s[tp]=1
+	if Duel.GetFlagEffect(tp,id)~=0 then return end
+	local e2=Effect.CreateEffect(e:GetHandler())
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_CHANGE_DAMAGE)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetTargetRange(1,0)
+	e2:SetValue(s.val)
+	e2:SetReset(RESET_PHASE+PHASE_END,1)
+	Duel.RegisterEffect(e2,tp)
+	Duel.RegisterFlagEffect(tp,id,RESET_PHASE+PHASE_END,0,1)
+end
+function s.val(e,re,dam,r,rp,rc)
+	if s[e:GetOwnerPlayer()]==1 or (r&REASON_BATTLE)~=0 then
+		return dam/2
+	else return dam end
+end
+--protect from battle
+function s.battleop(e,tp,eg,ep,ev,re,r,rp)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e1:SetTarget(aux.TargetBoolFunction(Card.IsRed))
+	e1:SetTargetRange(LOCATION_MZONE,0)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	e1:SetValue(1)
+	Duel.RegisterEffect(e1,tp)
+end
--- expansions/script/c333300080.lua
+++ expansions/script/c333300080.lua
@@ -10,93 +10,56 @@ function s.initial_effect(c)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
-    --Special Summon 1 "The Crimson Dragon" from your Extra Deck
-	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,0))
-	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e2:SetType(EFFECT_TYPE_IGNITION)
-	e2:SetRange(LOCATION_GRAVE)
-    e2:SetCountLimit(1,id)
-	e2:SetCondition(s.spcon)
-    e2:SetCost(s.spcost)
-	e2:SetTarget(s.sptg)
-	e2:SetOperation(s.spop)
-	c:RegisterEffect(e2)
+    --Special Summon 1 "Crimson Dragon" from your Extra Deck
+
 end
 s.listed_names={CARD_CRIMSON_DRAGON,CARD_RED_DRAGON_ARCHFIEND,id}
 --Special Summon Synchro Monster 
 function s.filter1(c,e,tp)
-	return c:IsType(TYPE_SYNCHRO) and (Duel.GetLocationCountFromEx(tp,tp,nil,c)>0 or Duel.IsPlayerAffectedByEffect(e:GetHandlerPlayer(),69832741))
-		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_SYNCHRO,tp,false,false)
-end
-function s.filter2(c,e,tp,sc)
-	local rg=Duel.GetMatchingGroup(s.filter3,tp,LOCATION_MZONE,0,c)
-	return c:IsType(TYPE_SYNCHRO) and c:IsAttribute(ATTRIBUTE_DARK) and c:IsRace(RACE_DRAGON) and c:IsAbleToGrave() and aux.SpElimFilter(c,true) 
-		and aux.SelectUnselectGroup(rg,e,tp,nil,nil,s.rescon(c,sc),0)
+	local lv=c:GetLevel()
+	return c:IsType(TYPE_SYNCHRO) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_SYNCHRO,tp,false,false)
+		and Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_MZONE,0,1,nil,tp,c)
 end
 function s.rescon(synchro,scard)
 	return	function(sg,e,tp,mg)
 				sg:AddCard(synchro)
-				local res=Duel.GetLocationCountFromEx(tp,tp,sg,scard)>0 and sg:CheckWithSumEqual(Card.GetLevel,scard:GetLevel(),#sg,#sg)
+				local res=Duel.GetLocationCountFromEx(tp,tp,sg,scard)>0 
+					and sg:CheckWithSumEqual(Card.GetLevel,scard:GetLevel(),#sg,#sg)
 				sg:RemoveCard(synchro)
 				return res
 			end
 end
+function s.filter2(c,e,tp,sc)
+	local rg=Duel.GetMatchingGroup(s.filter3,tp,LOCATION_MZONE,0,c)
+	return c:IsAttribute(ATTRIBUTE_DARK) and c:IsRace(RACE_DRAGON) and c:IsType(TYPE_SYNCHRO) and c:IsAbleToGrave() and aux.SpElimFilter(c,true) 
+		and aux.SelectUnselectGroup(rg,e,tp,nil,2,s.rescon(c,sc),0)
+end
 function s.filter3(c)
-	return c:HasLevel() and c:IsType(TYPE_TUNER) and c:IsRace(RACE_FIEND) and c:IsAbleToGrave() and aux.SpElimFilter(c,true)
+	return c:IsRace(RACE_FIEND) and c:IsType(TYPE_TUNER) and c:IsAbleToGrave() and aux.SpElimFilter(c,true)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter1,tp,LOCATION_EXTRA,0,1,nil,e,tp)
-        and Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_MZONE,0,1,nil,e,tp) end
-	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,2,tp,LOCATION_MZONE)
+	if chk==0 then
+		local pg=aux.GetMustBeMaterialGroup(tp,Group.CreateGroup(),tp,nil,nil,REASON_SYNCHRO)
+		return #pg<=0 and Duel.IsExistingMatchingCard(s.filter1,tp,LOCATION_EXTRA,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local pg=aux.GetMustBeMaterialGroup(tp,Group.CreateGroup(),tp,nil,nil,REASON_SYNCHRO)
-	if #pg<=0 and Duel.IsExistingMatchingCard(s.filter1,tp,LOCATION_EXTRA,0,1,nil,e,tp) then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local g1=Duel.SelectMatchingCard(tp,s.filter1,tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
-		local sc=g1:GetFirst()
+	if #pg>0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g1=Duel.SelectMatchingCard(tp,s.filter1,tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
+	local sc=g1:GetFirst()
+	if sc then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-		local g2=Duel.SelectMatchingCard(tp,s.filter2,tp,LOCATION_MZONE,0,1,1,nil,e,tp,sc)
+		local g2=Duel.SelectMatchingCard(tp,s.filter2,tp,LOCATION_MZONE,0,1,1,nil,tp,sc)
 		local synchro=g2:GetFirst()
 		local rg=Duel.GetMatchingGroup(s.filter3,tp,LOCATION_MZONE,0,synchro)
-		local sg=aux.SelectUnselectGroup(rg,e,tp,nil,nil,s.rescon(synchro,sc),1,tp,HINTMSG_TOGRAVE,s.rescon(synchro,sc))
+		local sg=aux.SelectUnselectGroup(rg,e,tp,1,2,s.rescon(synchro,sc),1,tp,HINTMSG_TOGRAVE,s.rescon(synchro,sc))
 		sg:AddCard(synchro)
 		Duel.SendtoGrave(sg,REASON_EFFECT)
 		Duel.SpecialSummon(sc,SUMMON_TYPE_SYNCHRO,tp,tp,false,false,POS_FACEUP)
 		sc:CompleteProcedure()
 	end
+	Duel.SpecialSummonComplete()
 end
 --Special Summon "The Crimson Dragon"
-function s.confilter(c)
-	return c:IsCode(CARD_RED_DRAGON_ARCHFIEND) or (c:ListsCode(CARD_RED_DRAGON_ARCHFIEND) and c:IsMonster())
-end
-function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(s.confilter,tp,LOCATION_GRAVE,0,1,nil)
-end
-function s.spcfilter(c,tp)
-	return c:IsLevelAbove(9) and c:IsType(TYPE_SYNCHRO) and c:IsAbleToExtraAsCost()
-end
-function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(s.spcfilter,tp,LOCATION_GRAVE,0,1,nil,tp) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-	local g=Duel.SelectMatchingCard(tp,s.spcfilter,tp,LOCATION_GRAVE,0,1,1,nil,tp)
-	Duel.SendtoDeck(g,nil,2,REASON_COST)
-    Duel.SendtoDeck(e:GetHandler(),nil,0,REASON_COST)
-end
-function s.spfilter(c,e,tp)
-	return c:IsCode(CARD_CRIMSON_DRAGON) and Duel.GetLocationCountFromEx(tp,tp,nil,c)>0
-		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-end
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
-end
-function s.spop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
-	if #g>0 then
-		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
-	end
-end
